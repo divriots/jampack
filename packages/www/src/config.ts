45a9719aeeea0aa879c1155cef10dc7ecda97100
Georges Gomes <georges.gomes@gmail.com>
2023-01-20 17:56:41 +0100

    better left panel in docs

--- packages/www/src/config.ts
+++ packages/www/src/config.ts
@@ -1,18 +1,18 @@
-import fs from "fs";
-import fm from "front-matter";
+import fs from 'fs';
+import fm from 'front-matter';
 
 export const SITE = {
-  title: "jampack",
-  description: "Your website description.",
-  defaultLanguage: "en_US",
+  title: 'jampack',
+  description: 'Your website description.',
+  defaultLanguage: 'en_US',
 };
 
 export const OPEN_GRAPH = {
   image: {
-    src: "/og-image.jpg",
-    alt: "",
+    src: '/og-image.jpg',
+    alt: '',
   },
-  twitter: "divRIOTS",
+  twitter: 'divRIOTS',
 };
 
 // This is the type of the frontmatter you put in the docs markdown files.
@@ -21,13 +21,13 @@ export type Frontmatter = {
   description: string;
   layout: string;
   image?: { src: string; alt: string };
-  dir?: "ltr" | "rtl";
+  dir?: 'ltr' | 'rtl';
   ogLocale?: string;
   lang?: string;
 };
 
 export const KNOWN_LANGUAGES = {
-  English: "en",
+  English: 'en',
 } as const;
 
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
@@ -38,9 +38,9 @@ export const COMMUNITY_INVITE_URL = `https://jampack.divriots.com/chat`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
-  indexName: "XXXXXXXXXX",
-  appId: "XXXXXXXXXX",
-  apiKey: "XXXXXXXXXX",
+  indexName: 'XXXXXXXXXX',
+  appId: 'XXXXXXXXXX',
+  apiKey: 'XXXXXXXXXX',
 };
 
 export type Sidebar = Record<
@@ -53,36 +53,36 @@ export type Sidebar = Record<
 //   .map(dirent => dirent.name);
 
 export const featuresDirs = [
-  "optimize-above-the-fold",
-  "optimize-images-to-webp",
-  "optimize-svg",
-  "responsive-images",
-  "set-image-dimensions",
-  "lazy-load-images",
-  "embed-small-images",
-  "compress-all",
-  "warnings",
+  'optimize-above-the-fold',
+  'optimize-images-to-webp',
+  'optimize-svg',
+  'responsive-images',
+  'set-image-dimensions',
+  'lazy-load-images',
+  'embed-small-images',
+  'compress-all',
+  'warnings',
 ];
 
 const getTitle = (file: string) => {
-  return fm(fs.readFileSync(file, "utf8")).attributes["title"];
+  return fm(fs.readFileSync(file, 'utf8')).attributes['title'];
 };
 
 export const SIDEBAR: Sidebar = {
   en: {
-    "Getting started": [{ text: "Introduction", link: "" }],
-    Reference: [
-      { text: "Installation", link: "installation" },
-      { text: "CLI Options", link: "cli-options" },
-      { text: "Configuration", link: "configuration" },
-      { text: "Cache", link: "cache" },
+    'Getting started': [
+      { text: 'Introduction', link: '' },
+      { text: 'Installation', link: 'installation' },
+      { text: 'CLI Options', link: 'cli-options' },
+      { text: 'Configuration', link: 'configuration' },
     ],
     Features: featuresDirs.map((dir) => ({
-      text: getTitle("./public/features/" + dir + "/index.md"),
-      link: "features/" + dir,
+      text: getTitle('./public/features/' + dir + '/index.md'),
+      link: 'features/' + dir,
     })),
+    Advanced: [, { text: 'Cache', link: 'cache' }],
     Community: [
-      { text: "Chat with us", link: "https://jampack.divriots.com/chat" },
+      { text: 'Chat with us', link: 'https://jampack.divriots.com/chat' },
     ],
   },
 };
