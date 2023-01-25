c0edff35c7201394bb3c9331fcb90efeb7de1b9b
Georges Gomes <georges.gomes@gmail.com>
2023-01-25 20:44:50 +0100

    refactor

--- src/optimize.ts
+++ src/optimize.ts
@@ -32,12 +32,23 @@ async function analyse(file: string): Promise<void> {
   const spinnerImg = ora({ prefixText: ' ' }).start();
   for (let i = 0; i < imgsArray.length; i++) {
     const imgElement = imgsArray[i];
+
     spinnerImg.text = kleur.dim(
       `<img> [${i + 1}/${imgsArray.length}] ${$(imgElement).attr('src')} `
     );
 
     const isAboveTheFold = imgElement.startIndex! < theFold;
-    await processImage(file, $, imgElement, isAboveTheFold);
+
+    try {
+      await processImage(file, $, imgElement, isAboveTheFold);
+    } catch (e) {
+      $state.reportIssue(file, {
+        type: 'erro',
+        msg:
+          (e as Error).message ||
+          `Unexpected error while processing image: ${JSON.stringify(e)}`,
+      });
+    }
   }
 
   // Reset spinner
@@ -91,312 +102,300 @@ async function processImage(
   imgElement: cheerio.Element,
   isAboveTheFold: boolean
 ): Promise<void> {
-  try {
-    const img = $(imgElement);
+  const img = $(imgElement);
 
-    /*
-     * Attribute 'src'
-     */
-    const attrib_src = img.attr('src');
-    if (!attrib_src) {
-      $state.reportIssue(htmlfile, {
-        type: 'warn',
-        msg: `Missing [src] on img - processing skipped.`,
-      });
-      return;
-    }
+  /*
+   * Attribute 'src'
+   */
+  const attrib_src = img.attr('src');
+  if (!attrib_src) {
+    $state.reportIssue(htmlfile, {
+      type: 'warn',
+      msg: `Missing [src] on img - processing skipped.`,
+    });
+    return;
+  }
 
-    /*
-     * Attribute 'alt'
-     */
-    const attrib_alt = img.attr('alt');
-    if (attrib_alt === undefined) {
-      $state.reportIssue(htmlfile, {
-        type: 'a11y',
-        msg: `Missing [alt] on img src="${attrib_src}" - Adding alt="" meanwhile.`,
-      });
-      img.attr('alt', '');
-    }
+  /*
+   * Attribute 'alt'
+   */
+  const attrib_alt = img.attr('alt');
+  if (attrib_alt === undefined) {
+    $state.reportIssue(htmlfile, {
+      type: 'a11y',
+      msg: `Missing [alt] on img src="${attrib_src}" - Adding alt="" meanwhile.`,
+    });
+    img.attr('alt', '');
+  }
 
-    if (attrib_src.startsWith('data:')) {
-      // Data URI image
-      // TODO: try to compress it
-      return;
-    }
+  if (attrib_src.startsWith('data:')) {
+    // Data URI image
+    // TODO: try to compress it
+    return;
+  }
 
-    /*
-     * Attribute 'loading'
-     */
-    const attr_loading = img.attr('loading');
-    if (isAboveTheFold) {
-      img.removeAttr('loading');
-      img.attr('fetchpriority', 'high');
-    } else {
-      switch (attr_loading) {
-        case undefined:
-          // Go lazy by default
-          img.attr('loading', 'lazy');
-          break;
-        case 'eager':
-          img.removeAttr('loading');
-          break;
-        case 'lazy':
-          // Don't touch it
-          break;
-        default:
-          $state.reportIssue(htmlfile, {
-            type: 'invalid',
-            msg: `Invalid [loading]="${attr_loading}" on img src="${attrib_src}"`,
-          });
-      }
-    }
-
-    /*
-     * Attribute 'decoding'
-     */
-    img.attr('decoding', 'async');
-
-    if (!isLocal(attrib_src)) {
-      // Image not local, don't touch it
-      return;
-    }
-
-    /*
-     * Loading image
-     */
-
-    const originalImage = await Resource.loadResource(
-      $state.dir,
-      htmlfile,
-      attrib_src
-    );
-
-    // No file -> give up
-    if (!originalImage) {
-      $state.reportIssue(htmlfile, {
-        type: 'erro',
-        msg: `Can't find img on disk src="${attrib_src}"`,
-      });
-      return;
-    }
-
-    /*
-     * Compress image
-     */
-    let newImage: Image | undefined;
-
-    const originalImageMeta = await originalImage.getImageMeta();
-    const canBeProgressiveJpeg =
-      isAboveTheFold && originalImageMeta && !originalImageMeta.hasAlpha;
-
-    if (!$state.optimizedFiles.has(originalImage.filePathAbsolute)) {
-      // Let's avoid to optimize same images twice
-      $state.optimizedFiles.add(originalImage.filePathAbsolute);
-
-      newImage = await compressImage(await originalImage.getData(), {
-        toFormat: canBeProgressiveJpeg ? 'pjpg' : 'webp',
-      });
-
-      if (
-        newImage?.data &&
-        (newImage.data.length < (await originalImage.getLen()) ||
-          canBeProgressiveJpeg) // Progressive jpg above the fold should get replaced even if bigger
-      ) {
-        // Do we need to add an new extension?
-        const newExtension = `.${newImage.format}`;
-        const additionalExtension =
-          path.extname(originalImage.filePathAbsolute) === newExtension
-            ? ''
-            : newExtension;
-
-        const newFilename =
-          originalImage.filePathAbsolute + additionalExtension;
-
-        if (!$state.args.nowrite) {
-          fs.writeFile(newFilename, newImage.data);
-        }
-
-        // Mark new file as optimized
-        $state.compressedFiles.add(newFilename);
-
-        // Report compression result
-        $state.reportSummary({
-          action:
-            newFilename !== originalImage.filePathAbsolute
-              ? `${await originalImage.getExt()}->${newImage.format}`
-              : path.extname(originalImage.filePathAbsolute),
-          originalSize: await originalImage.getLen(),
-          compressedSize: newImage.data.length,
-        });
-
-        img.attr('src', attrib_src + additionalExtension);
-      } else {
-        // Report non-compression
-        $state.reportSummary({
-          action: path.extname(originalImage.filePathAbsolute),
-          originalSize: await originalImage.getLen(),
-          compressedSize: await originalImage.getLen(),
-        });
-      }
-    }
-
-    /*
-     * Embed small images
-     *
-     * TODO this is only embedding images that have
-     * successfully be compressed. Should embed original
-     * image if it fits the size.
-     */
-    let isEmbed = false;
-    if (newImage && newImage.data.length <= config.image.embed_size) {
-      let datauri = undefined;
-
-      switch (newImage.format) {
-        case 'svg':
-          datauri = svgToMiniDataURI(newImage.data.toString());
-          break;
-        case 'webp':
-          datauri = `data:image/webp;base64,${newImage.data.toString(
-            'base64'
-          )}`;
-          break;
-        case 'jpg':
-        case 'png':
-          // TODO but not possible in current code
-          break;
-      }
-
-      if (datauri) {
-        isEmbed = true;
-        img.attr('src', datauri);
+  /*
+   * Attribute 'loading'
+   */
+  const attr_loading = img.attr('loading');
+  if (isAboveTheFold) {
+    img.removeAttr('loading');
+    img.attr('fetchpriority', 'high');
+  } else {
+    switch (attr_loading) {
+      case undefined:
+        // Go lazy by default
+        img.attr('loading', 'lazy');
+        break;
+      case 'eager':
         img.removeAttr('loading');
-        img.removeAttr('decoding');
-
-        $state.reportSummary({
-          action: `${newImage.format}->embed`,
-          originalSize: await originalImage.getLen(),
-          compressedSize: newImage.data.length,
+        break;
+      case 'lazy':
+        // Don't touch it
+        break;
+      default:
+        $state.reportIssue(htmlfile, {
+          type: 'invalid',
+          msg: `Invalid [loading]="${attr_loading}" on img src="${attrib_src}"`,
         });
-      }
     }
+  }
 
-    if (isEmbed) {
-      // Image is embed, no need for more processing
-      return;
-    }
+  /*
+   * Attribute 'decoding'
+   */
+  img.attr('decoding', 'async');
 
-    /*
-     * Attribute 'width' & 'height'
-     */
-    const [w, h] = await setImageSize(htmlfile, img, originalImage);
+  if (!isLocal(attrib_src)) {
+    // Image not local, don't touch it
+    return;
+  }
 
-    //
-    // Stop here if svg
-    //
-    if ((await originalImage.getExt()) === 'svg') {
-      return;
-    }
+  /*
+   * Loading image
+   */
 
-    /*
-     * Attribute 'srcset'
-     */
-    if (img.attr('srcset')) {
-      // If srcset are set, don't touch it.
-      // The compress pass will compress the images
-      // of the srcset
-    } else {
-      // Generate image set
-      // Avif => Avif
-      // png, jpg, ... => WebP
-      const targetFormat =
-        (await originalImage.getMime()) === 'image/avif' ? 'avif' : 'webp';
+  const originalImage = await Resource.loadResource(
+    $state.dir,
+    htmlfile,
+    attrib_src
+  );
 
-      const ext = path.extname(attrib_src);
-      const fullbasename = attrib_src.slice(0, -ext.length);
-      const imageSrc = (addition: string) =>
-        `${fullbasename}${addition}.${
-          canBeProgressiveJpeg ? 'jpg' : targetFormat
-        }`;
-
-      // Start from original image
-      let new_srcset = '';
-
-      // Start reduction
-      const step = 300; //px
-      const ratio = w / h;
-      let valueW = w - step;
-      let valueH = Math.trunc(valueW / ratio);
-      let previousImageSize = newImage?.data
-        ? Math.min(newImage.data.length, await originalImage.getLen())
-        : await originalImage.getLen();
-
-      while (valueW >= config.image.srcset_min_width) {
-        const src = imageSrc(`@${valueW}w`);
-
-        const absoluteFilename = translateSrc(
-          $state.dir,
-          path.dirname(htmlfile),
-          src
-        );
-
-        let doAddToSrcSet = true;
-
-        // Don't generate srcset file twice
-        if (!$state.compressedFiles.has(absoluteFilename)) {
-          const compressedImage = await compressImage(
-            await originalImage.getData(),
-            {
-              resize: { width: valueW, height: valueH },
-              toFormat: canBeProgressiveJpeg ? 'pjpg' : targetFormat,
-            }
-          );
-
-          if (
-            !compressedImage?.data ||
-            compressedImage.data.length >= previousImageSize
-          ) {
-            // New image is not compressed or bigger than previous image in srcset
-            // Don't add to srcset
-            doAddToSrcSet = false;
-          } else {
-            // Write file
-            if (!$state.args.nowrite) {
-              fs.writeFile(absoluteFilename, compressedImage.data);
-            }
-
-            // Set new previous size to beat
-            previousImageSize = compressedImage.data.length;
-
-            // Add file to list avoid recompression
-            $state.compressedFiles.add(absoluteFilename);
-          }
-        }
-
-        if (doAddToSrcSet) {
-          new_srcset += `, ${src} ${valueW}w`;
-        }
-
-        // reduce size
-        valueW -= step;
-        valueH = Math.trunc(valueW / ratio);
-      }
-
-      if (new_srcset) {
-        img.attr('srcset', `${img.attr('src')} ${w}w` + new_srcset);
-      }
-
-      // Add sizes attribute if not specified
-      if (!img.attr('sizes')) {
-        img.attr('sizes', '100vw');
-      }
-    }
-  } catch (e) {
+  // No file -> give up
+  if (!originalImage) {
     $state.reportIssue(htmlfile, {
       type: 'erro',
-      msg:
-        (e as Error).message ||
-        `Unexpected error while processing image: ${JSON.stringify(e)}`,
+      msg: `Can't find img on disk src="${attrib_src}"`,
     });
+    return;
+  }
+
+  /*
+   * Compress image
+   */
+  let newImage: Image | undefined;
+
+  const originalImageMeta = await originalImage.getImageMeta();
+  const canBeProgressiveJpeg =
+    isAboveTheFold && originalImageMeta && !originalImageMeta.hasAlpha;
+
+  if (!$state.optimizedFiles.has(originalImage.filePathAbsolute)) {
+    // Let's avoid to optimize same images twice
+    $state.optimizedFiles.add(originalImage.filePathAbsolute);
+
+    newImage = await compressImage(await originalImage.getData(), {
+      toFormat: canBeProgressiveJpeg ? 'pjpg' : 'webp',
+    });
+
+    if (
+      newImage?.data &&
+      (newImage.data.length < (await originalImage.getLen()) ||
+        canBeProgressiveJpeg) // Progressive jpg above the fold should get replaced even if bigger
+    ) {
+      // Do we need to add an new extension?
+      const newExtension = `.${newImage.format}`;
+      const additionalExtension =
+        path.extname(originalImage.filePathAbsolute) === newExtension
+          ? ''
+          : newExtension;
+
+      const newFilename = originalImage.filePathAbsolute + additionalExtension;
+
+      if (!$state.args.nowrite) {
+        fs.writeFile(newFilename, newImage.data);
+      }
+
+      // Mark new file as optimized
+      $state.compressedFiles.add(newFilename);
+
+      // Report compression result
+      $state.reportSummary({
+        action:
+          newFilename !== originalImage.filePathAbsolute
+            ? `${await originalImage.getExt()}->${newImage.format}`
+            : path.extname(originalImage.filePathAbsolute),
+        originalSize: await originalImage.getLen(),
+        compressedSize: newImage.data.length,
+      });
+
+      img.attr('src', attrib_src + additionalExtension);
+    } else {
+      // Report non-compression
+      $state.reportSummary({
+        action: path.extname(originalImage.filePathAbsolute),
+        originalSize: await originalImage.getLen(),
+        compressedSize: await originalImage.getLen(),
+      });
+    }
+  }
+
+  /*
+   * Embed small images
+   *
+   * TODO this is only embedding images that have
+   * successfully be compressed. Should embed original
+   * image if it fits the size.
+   */
+  let isEmbed = false;
+  if (newImage && newImage.data.length <= config.image.embed_size) {
+    let datauri = undefined;
+
+    switch (newImage.format) {
+      case 'svg':
+        datauri = svgToMiniDataURI(newImage.data.toString());
+        break;
+      case 'webp':
+        datauri = `data:image/webp;base64,${newImage.data.toString('base64')}`;
+        break;
+      case 'jpg':
+      case 'png':
+        // TODO but not possible in current code
+        break;
+    }
+
+    if (datauri) {
+      isEmbed = true;
+      img.attr('src', datauri);
+      img.removeAttr('loading');
+      img.removeAttr('decoding');
+
+      $state.reportSummary({
+        action: `${newImage.format}->embed`,
+        originalSize: await originalImage.getLen(),
+        compressedSize: newImage.data.length,
+      });
+    }
+  }
+
+  if (isEmbed) {
+    // Image is embed, no need for more processing
+    return;
+  }
+
+  /*
+   * Attribute 'width' & 'height'
+   */
+  const [w, h] = await setImageSize(htmlfile, img, originalImage);
+
+  //
+  // Stop here if svg
+  //
+  if ((await originalImage.getExt()) === 'svg') {
+    return;
+  }
+
+  /*
+   * Attribute 'srcset'
+   */
+  if (img.attr('srcset')) {
+    // If srcset are set, don't touch it.
+    // The compress pass will compress the images
+    // of the srcset
+  } else {
+    // Generate image set
+    // Avif => Avif
+    // png, jpg, ... => WebP
+    const targetFormat =
+      (await originalImage.getMime()) === 'image/avif' ? 'avif' : 'webp';
+
+    const ext = path.extname(attrib_src);
+    const fullbasename = attrib_src.slice(0, -ext.length);
+    const imageSrc = (addition: string) =>
+      `${fullbasename}${addition}.${
+        canBeProgressiveJpeg ? 'jpg' : targetFormat
+      }`;
+
+    // Start from original image
+    let new_srcset = '';
+
+    // Start reduction
+    const step = 300; //px
+    const ratio = w / h;
+    let valueW = w - step;
+    let valueH = Math.trunc(valueW / ratio);
+    let previousImageSize = newImage?.data
+      ? Math.min(newImage.data.length, await originalImage.getLen())
+      : await originalImage.getLen();
+
+    while (valueW >= config.image.srcset_min_width) {
+      const src = imageSrc(`@${valueW}w`);
+
+      const absoluteFilename = translateSrc(
+        $state.dir,
+        path.dirname(htmlfile),
+        src
+      );
+
+      let doAddToSrcSet = true;
+
+      // Don't generate srcset file twice
+      if (!$state.compressedFiles.has(absoluteFilename)) {
+        const compressedImage = await compressImage(
+          await originalImage.getData(),
+          {
+            resize: { width: valueW, height: valueH },
+            toFormat: canBeProgressiveJpeg ? 'pjpg' : targetFormat,
+          }
+        );
+
+        if (
+          !compressedImage?.data ||
+          compressedImage.data.length >= previousImageSize
+        ) {
+          // New image is not compressed or bigger than previous image in srcset
+          // Don't add to srcset
+          doAddToSrcSet = false;
+        } else {
+          // Write file
+          if (!$state.args.nowrite) {
+            fs.writeFile(absoluteFilename, compressedImage.data);
+          }
+
+          // Set new previous size to beat
+          previousImageSize = compressedImage.data.length;
+
+          // Add file to list avoid recompression
+          $state.compressedFiles.add(absoluteFilename);
+        }
+      }
+
+      if (doAddToSrcSet) {
+        new_srcset += `, ${src} ${valueW}w`;
+      }
+
+      // reduce size
+      valueW -= step;
+      valueH = Math.trunc(valueW / ratio);
+    }
+
+    if (new_srcset) {
+      img.attr('srcset', `${img.attr('src')} ${w}w` + new_srcset);
+    }
+
+    // Add sizes attribute if not specified
+    if (!img.attr('sizes')) {
+      img.attr('sizes', '100vw');
+    }
   }
 }
 
